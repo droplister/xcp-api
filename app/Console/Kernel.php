7b700c1e14e1f59128129f91d684db5f1ad761dc
Dan Anderson <anderson@familymedia.org>
2019-04-04 20:14:35 -0400

    Update Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,13 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        // Useful Switch
+        if (config('xcp-core.indexing')) {
+            $schedule->command('update:index')->everyMinute();
+        }
+
+        // Horizon Stats
+        $schedule->command('horizon:snapshot')->everyFiveMinutes();
     }
 
     /**
